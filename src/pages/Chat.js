c51a9bbb6906159bdcd0865e27971cd9768d31de
Matheus Monteiro Alves <matheus.alves@intelie.com.br>
2021-02-16 16:29:54 -0300

    exemple chat

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,12 +1,65 @@
-import * as React from "react";
-import { MaterialIcons } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-import { Text, View } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import { Text, View, Button, Alert } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default function Chat() {
-  return(
-  <View style={{backgroundColor:'#98F', flex: 1}}>
-    
-    
-  </View>)}
+  const [chat, setChat] = useState(false);
+
+  if (chat == false) {
+    return <ChatMessage setChat={setChat} chat={chat}/>;
+  }
+
+  return (
+    <View style={{ backgroundColor: "#98F", flex: 1, marginTop: 30 }}>
+      <Button
+        onPress={() => setChat(!chat)}
+        title="Inicia Chat"
+        color="#6A5ACD"
+      />
+    </View>
+  );
+}
+
+export function ChatMessage({setChat, chat}) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  return (
+    <>
+    <View style={{marginTop: 30}}>
+      <Button
+        onPress={() => setChat(!chat)}
+        title="Voltar"
+        color="#6A5ACD"
+      />
+      </View>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    </>
+  );
+}
